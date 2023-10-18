f938e1428db2c7735e3ca60480a03aa5132330ba
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-10-18 17:34:33 +0200

    Ejercicio 2 hecho

---
+++ Tema6_MDV/Tema6_MDV/Controllers/ProductosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tema6_MDV.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ListadoProductos()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tema6_MDV/Tema6_MDV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tema6_MDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
