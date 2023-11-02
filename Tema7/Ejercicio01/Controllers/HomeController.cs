c704a0b4a9be2c4ca5995477f63b1519ce8b92fe
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-11-02 09:26:10 +0100

    No fufa los datos del modelo

---
+++ Tema7/Ejercicio01/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Ejercicio01.Models;
+using Ejercicio01.Models.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio01.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ActionResult Index()
+        {
+            var fechaActual=DateTime.Now;
+            var mensaje = "Buenos días";
+
+            if (fechaActual.Hour>=12)
+            {
+                mensaje = "Buenas tardes";
+
+            } else if (fechaActual.Hour>=21)
+            {
+                mensaje = "Buenas noches";
+            }
+
+            ViewData["Mensaje"] = mensaje;
+            ViewBag.fecha = fechaActual.ToLongDateString();
+            return View();
+        }
+        private clsPersona persona = new clsPersona()
+        {
+            Id = 1,
+            Nombre = "Isabel Katharina",
+            Apellidos = "Loerzer",
+            Direccion = "Av de la Paz",
+            FechaNac = new DateTime(1998, 12, 05),
+            Telefono = 654444089,
+        };
+    }
+}
