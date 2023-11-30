4d08d05b5c37e8e92b86677a7d345b8661caa07d
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-11-30 14:33:51 +0100

    Proyecto de maui con el listado

---
+++ Tema10/CRUD/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CRUD
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+     
+    }
+}
---
+++ Tema10/CRUD/ViewModels/MainPageVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+using BL;
+
+namespace CRUD.ViewModels
+{
+    internal class MainPageVM
+    {
+        private ObservableCollection<clsPersona> listadoCompletoPersonas;
+
+        public MainPageVM()
+        {
+            try
+            {
+                listadoCompletoPersonas = new ObservableCollection<clsPersona>(clsListaPersonasBL.listadoCompletoPersonasBL());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            
+        }
+
+        public ObservableCollection<clsPersona> ListadoCompletoPersonas
+        {
+            get { return listadoCompletoPersonas; }
+        }
+
+
+    }
+}
