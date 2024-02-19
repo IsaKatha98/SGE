883ec9cd87caf8f0ba7f58d93c4f749b858d048a
IsaKatha98 <isabel.loerzer@iesnervion.es>
2024-02-19 22:53:12 +0100

    bof

--- Tema13/CRUD_AJAX/ajax.js
+++ Tema13/CRUD_AJAX/ajax.js
@@ -1,4 +1,4 @@
-window.onload=first; //no podemos llamar a la función directamente porque se ejectuaría
+window.onload=first;
 
 var tablePeople=document.getElementById("maintable")
 var buttonNew= document.getElementById("btnNew");
@@ -6,17 +6,70 @@ var buttonEdit= document.getElementById("btnEdit");
 var buttonDelete= document.getElementById("btnDelete");
 var rowsPeople= document.getElementById("rows"); //tbody
 var newRow=document.getElementsByTagName("tr");
+var listDept=[] //variable that saves a list of departments
+
+//classes
+class Persona{
+
+    constructor (p) {
+        this.id=p.id;
+        this.nombre=p.nombre;
+        this.apellidos=p.apellidos;
+        this.direccion=p.direccion;
+        this.tlf=p.tlf;
+        this.fotoURL=p.fotoURL;
+        this.fechaNac=p.fechaNac;
+        this.idDepartamento=p.idDepartamento;
+    }
+}
+
+class Departamento {
+    constructor (id, nombre) {
+        this.id=id;
+        this.nombre=nombre;
+    }
+}
+
+class PersonaconnombreDepartamento extends Persona{
+
+    constructor(p){
+        let nombreDepartamento="Departamento no asignado";
+        super(p);
+        for (var i=0; i<listDept.length;i++) {
+
+            if (p.idDepartamento==listDept[i].idDepartamento) {
+                
+                nombreDepartamento=listaDept[i].nombre;
+                break;
+            }
+        }
+
+        this.nombreDepartamento=nombreDepartamento;
+        
+        
+    } 
+
+}
 
 function first() {
 
-    getsIndex()
-    //each row of the table can be clicked
-  
-    rowsPeople.addEventListener("click", selected, false);
-    //edite and delete buttons will be hidden
+    //load the list of departments
+    //we make a promise that there is going to be a list of departments.
+    getsDeptList()
+    .then(()=>{
+        getsIndex();
+
+        //each row can be clicked
+        rowsPeople.addEventListener("click", selected, false);
+        //edit and delete buttons will be hidden
     //these buttons will only show when a person is selected
     buttonDelete.style.display="none";
     buttonEdit.style.display="none";
+    })
+    .catch(error=>{
+        console.error("Error fetching department list:", error);
+    })
+    
     
 }
 
@@ -45,7 +98,8 @@ function getsIndex () {
         for (var i=0; i<data.length;i++) {
 
             //creates a person
-            var person= data[i];
+            var oPersona= new PersonaconnombreDepartamento(data[i])
+             
             //creates a row for every person in the list.
             var newRow=rowsPeople.insertRow();
 
@@ -59,13 +113,13 @@ function getsIndex () {
             var cellDept=newRow.insertCell(6);
 
             //adds value to each cell.
-            cellName.innerHTML=person.nombre;
-            cellSurname.innerHTML=person.apellidos;
-            cellAddress.innerHTML=person.direccion;
-            cellPhone.innerHTML=person.tlf;
-            cellPic.innerHTML=person.fotoURL;
-            cellBirthDate.innerHTML=person.fechaNac;
-            cellDept.innerHTML=person.idDepartamento;
+            cellName.innerHTML=oPersona.nombre;
+            cellSurname.innerHTML=oPersona.apellidos;
+            cellAddress.innerHTML=oPersona.direccion;
+            cellPhone.innerHTML=oPersona.tlf;
+            cellPic.innerHTML=oPersona.fotoURL;
+            cellBirthDate.innerHTML=oPersona.fechaNac;
+            cellDept.innerHTML=oPersona.nombreDepartamento;
         }
         
     })
@@ -78,11 +132,59 @@ function getsIndex () {
 
 }
 
+function getsDeptList () {
+    return new Promise(function(resolve, reject){
+
+        var url="https://crudisasegundo.azurewebsites.net/api/departamentos";
+
+        //we fetch the url
+        fetch(url)
+        .then(response=>{
+            
+            //confirm thar response was a success
+            if (!response.ok) {
+                //if not, it throws an error
+                throw new Error("Not working");
+            
+            }
+    
+            return response.json();
+        })
+        //manage the data that comes from the API
+        .then(data => {
+   
+            //resolves promise
+            listDept=data;
+            resolve();
+        })
+        
+        //if there are errors.
+        .catch(error=>{
+
+            //rejects promise because of error
+            reject(error)
+
+        })
+
+    })
+   
+}
+
 
 
 function selected() {
 
     //gets the selected person.
     alert("Se ha seleccionado una persona")
+
+    const row=selected.target.closest("tr");
+
+    if (row) {
+        if (beforeSelectedRow) {
+            beforeSelectedRow.style.backgroundColor="";
+        }
+        beforeSelectedRow=row;
+        row.style.backgroundColor="#CBC8C8";
+    }
 
 }
